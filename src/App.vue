73d3a1af0e9ebd7c1cdd2c4a1df052c3d6a284f1
jeromehaas <hello@jeromehaas.ch>
2024-08-29 12:51:25 +0200

    Cleanup: project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+
 // IMPORTS
 import './styles/main.scss';
 import {RouterView } from 'vue-router'
+
 </script>
 
 <template>
--- src/components/partials/score/score.vue
+++ src/components/partials/score/score.vue
@@ -1,6 +1,9 @@
 <script setup>
+
+// IMPORTS
 import './score.scss';
 import gameStore from '@/stores/game-store.js';
+
 </script>
 
 <template>
@@ -8,18 +11,14 @@ import gameStore from '@/stores/game-store.js';
     <div class="score__player">
       <img class="score__player-label" src="/icons/icon-player-x.svg"></img>
       <div class="score__player-points">
-        <img class="score__player-points-stroke-group" v-for="score in parseInt(gameStore.state.score['X'] / 5)"
-             src="/icons/icon-stroke-group.svg"/>
-        <img class="score__player-points-strokes" v-for="score in parseInt(gameStore.state.score['X'] % 5)"
-             src="/icons/icon-stroke.svg"/>
+        <img class="score__player-points-stroke-group" v-for="score in parseInt(gameStore.state.score['X'] / 5)" src="/icons/icon-stroke-group.svg"/>
+        <img class="score__player-points-strokes" v-for="score in parseInt(gameStore.state.score['X'] % 5)" src="/icons/icon-stroke.svg"/>
       </div>
     </div>
     <div class="score__player">
       <div class="score__player-points">
-        <img class="score__player-points-strokes" v-for="score in parseInt(gameStore.state.score['O'] % 5)"
-             src="/icons/icon-stroke.svg"/>
-        <img class="score__player-points-stroke-group" v-for="score in parseInt(gameStore.state.score['O'] / 5)"
-             src="/icons/icon-stroke-group.svg"/>
+        <img class="score__player-points-strokes" v-for="score in parseInt(gameStore.state.score['O'] % 5)" src="/icons/icon-stroke.svg"/>
+        <img class="score__player-points-stroke-group" v-for="score in parseInt(gameStore.state.score['O'] / 5)" src="/icons/icon-stroke-group.svg"/>
       </div>
       <img class="score__player-label" src="/icons/icon-player-o.svg"></img>
     </div>
