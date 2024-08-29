34cb2b9caa9837edbca11558f33f4347d47235a8
jeromehaas <hello@jeromehaas.ch>
2024-08-29 12:26:30 +0200

    Setup: score partial

---
+++ src/components/partials/score/score.vue
@@ -0,0 +1,27 @@
+<script setup>
+import './score.scss';
+import gameStore from '@/stores/game-store.js';
+</script>
+
+<template>
+  <div class="score">
+    <div class="score__player">
+      <img class="score__player-label" src="/icons/icon-player-x.svg"></img>
+      <div class="score__player-points">
+        <img class="score__player-points-stroke-group" v-for="score in parseInt(gameStore.state.score['X'] / 5)"
+             src="/icons/icon-stroke-group.svg"/>
+        <img class="score__player-points-strokes" v-for="score in parseInt(gameStore.state.score['X'] % 5)"
+             src="/icons/icon-stroke.svg"/>
+      </div>
+    </div>
+    <div class="score__player">
+      <div class="score__player-points">
+        <img class="score__player-points-strokes" v-for="score in parseInt(gameStore.state.score['O'] % 5)"
+             src="/icons/icon-stroke.svg"/>
+        <img class="score__player-points-stroke-group" v-for="score in parseInt(gameStore.state.score['O'] / 5)"
+             src="/icons/icon-stroke-group.svg"/>
+      </div>
+      <img class="score__player-label" src="/icons/icon-player-o.svg"></img>
+    </div>
+  </div>
+</template>
--- src/components/sections/game/game.vue
+++ src/components/sections/game/game.vue
@@ -6,13 +6,17 @@ import Page from '@/components/layouts/page/page.vue';
 import Board from '@/components/partials/board/board.vue';
 import Moderator from '@/components/partials/moderator/moderator.vue';
 import Actions from '@/components/partials/actions/actions.vue';
+import Score from '@/components/partials/score/score.vue';
+import Tooltip from '@/components/partials/tooltip/tooltip.vue';
 
 </script>
 
 <template>
   <Page class="game">
+    <Tooltip class="game__tooltip" />
     <Moderator class="game__moderator" />
     <Board class="game__board" />
     <Actions class="game__actions" />
+    <Score class="game__score" />
   </Page>
 </template>
