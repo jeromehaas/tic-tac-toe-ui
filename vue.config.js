8000b6b6fd2ea6edf528d46375ea9f9aae7de11c
jeromehaas <hello@jeromehaas.ch>
2024-08-29 12:41:00 +0200

    Setup: vue config

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// CONFIGS
+const configs = {
+	devServer: {
+		host: '0.0.0.0',
+		port: 8080,
+		hot: true,
+		watchOptions: {
+			poll: true,
+		}
+	}
+};
+
+// EXPORTS
+module.exports = configs;
