64de83938db74f21e482d848bedb0d646b44984b
jeromehaas <hello@jeromehaas.ch>
2024-08-29 12:00:06 +0200

    Setup: game section

---
+++ src/components/sections/game/game.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+// IMPORTS
+import './game.scss';
+import Page from '@/components/layouts/page/page.vue';
+
+</script>
+
+<template>
+  <Page class="game">
+  </Page>
+</template>
