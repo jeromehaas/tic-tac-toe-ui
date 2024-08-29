305a547d29a011611a130ce4095c89442fc3836b
jeromehaas <hello@jeromehaas.ch>
2024-08-29 12:05:26 +0200

    Setup: router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 // IMPORTS
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import GameView from '@/views/game-view.vue';
 
 // ROUTER
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'game',
+			component: GameView
+		}
+	]
+});
 
 // EXPORTS
-export default router
+export default router;
